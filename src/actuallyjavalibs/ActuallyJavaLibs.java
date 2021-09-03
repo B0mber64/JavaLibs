62f6ce3e9eb20a3bf80f9e99b48adfd7287917ff
Landen <20634@my4county.net>
2021-09-03 09:07:47 -0400

    Making the thing

---
+++ src/actuallyjavalibs/ActuallyJavaLibs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actuallyjavalibs;
+import java.util.Scanner;
+/**
+ *
+ * @author lnickloy
+ */
+public class ActuallyJavaLibs {
+    Scanner sc = new Scanner(System.in);
+    /**
+     * JavaLibs assignment for Mr Geiger
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    public void noun(){
+    System.out.println("input a noun");
+    
+}
+    
+}
