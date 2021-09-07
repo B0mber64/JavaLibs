f3cda0c7e5241ae87bd6fc927e0d485012949a8a
Landen <20634@my4county.net>
2021-09-07 11:34:35 -0400

    finished

--- src/actuallyjavalibs/ActuallyJavaLibs.java
+++ src/actuallyjavalibs/ActuallyJavaLibs.java
@@ -24,7 +24,7 @@ public class ActuallyJavaLibs {
         String noun[]=new String[9];
         String verb[]=new String[9];
         String adjective[]=new String[9];
-        String number[]=new String[9];
+        int number[]=new int[9];
         System.out.println("Hello "+yourName+", are you ready for madness?");
         do{
             String response=sc.nextLine();
@@ -35,15 +35,40 @@ public class ActuallyJavaLibs {
         //MadLib inputs start here <---
             System.out.println("Please input the following:");
             System.out.println("noun (plural): ");
-            
+            noun[0]=sc.nextLine();
             System.out.println("noun (plural): ");
-            
+            noun[1]=sc.nextLine();
             System.out.println("noun (plural, preferably a weapon): ");
-            
+            noun[2]=sc.nextLine();
             System.out.println("noun (singular, preferably a weapon): ");
+            noun[3]=sc.nextLine();
             
+            System.out.println("verb: ");
+            verb[0]=sc.nextLine();
+            System.out.println("verb (ing): ");
+            verb[1]=sc.nextLine();
+            System.out.println("verb: ");
+            verb[2]=sc.nextLine();
             
+            System.out.println("adjective: ");
+            adjective[0]=sc.nextLine();
+            System.out.println("adjective: ");
+            adjective[1]=sc.nextLine();
+            System.out.println("adjective: ");
+            adjective[2]=sc.nextLine();
+            System.out.println("adjective: ");
+            adjective[3]=sc.nextLine();
+            System.out.println("adjective: ");
+            adjective[4]=sc.nextLine();
             
+            System.out.println("number: ");
+            number[0]=sc.nextInt();
+            System.out.println("number: ");
+            number[1]=sc.nextInt();
+            System.out.println("number (less than the first number): ");
+            number[2]=sc.nextInt();
+            
+            int remaining= number[0]-number[2];
             
         //Actual MadLib story <---
             System.out.println("On one "+adjective[0]+" day, "+yourName+
@@ -58,6 +83,8 @@ public class ActuallyJavaLibs {
                     +yourName+" a "+noun[3]+".");
             System.out.println("Filled with "+adjective[4]+" they "+verb[2]+" into battle.");
             System.out.println("In the end, the "+number[0]+" "+noun[0]+" overcame the "+number[1]);
+            System.out.println("Sadly, "+number[2]+" of the "+noun[0]+" were lost in battle.");
+            System.out.println("The remaining "+remaining+" "+noun[0]+" stand victorious");
             }
             else if(response.compareTo("no")==0){
             System.out.println("Why are you still here? Begone with you.");
